344d432d20a42ebab631c58f853942280d68e0e4
kaiing <315258157@qq.com>
2018-01-23 10:06:30 +0800

    add some function

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main() {
 	cout << "my first star" << endl;
+	cout << "i want to add the output" << endl;
+	int a, b;
+	a = 3, b = 4;
+	cout << "a + b = " << a + b << endl;
 	return 0;
 }
