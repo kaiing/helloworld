4cbce8e8249facffdf62b1db179e10f6ca4350a4
kaiing <315258157@qq.com>
2018-01-23 10:18:05 +0800

    alotofstring

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main() {
 	cout << "my first star" << endl;
+	cout << "i want to add the output" << endl;
+	int a, b;
+	a = 3, b = 4;
+	cout << "a + b = " << a + b << endl;
+	cout << "sfidfhdhfhdfh" << endl;
+	cout << "a + b = " << a + b << endl;
+	cout << "sfidfhdhfhdfh" << endl;
+	cout << "a + b = " << a + b << endl;
+	cout << "sfidfhdhfhdfh" << endl;
 	return 0;
 }
